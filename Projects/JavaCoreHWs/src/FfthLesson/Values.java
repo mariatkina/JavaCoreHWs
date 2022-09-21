ad27f9df561fe5f6a961a1ebeada539f125fa74c
EvgeneM <e.mariatkina@gmail.com>
2022-09-21 16:37:00 +0300

    ДЗ к уроку 5

---
+++ Projects/JavaCoreHWs/src/FfthLesson/Values.java
@@ -0,0 +1,25 @@
+package FfthLesson;
+
+public class Values {
+    private Integer value1;
+    private Integer value2;
+    private Integer value3;
+
+    public Values(Integer value1, Integer value2, Integer value3) {
+        this.value1 = value1;
+        this.value2 = value2;
+        this.value3 = value3;
+    }
+
+    public Integer getValue1() {
+        return value1;
+    }
+
+    public Integer getValue2() {
+        return value2;
+    }
+
+    public Integer getValue3() {
+        return value3;
+    }
+}
